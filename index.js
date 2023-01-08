b977439d1252c1b24a7ebd5c0c75eef3c458ad3b
Victor Hugo <victorhugofny@gmail.com>
2023-01-08 16:25:09 -0300

    primeiros comandos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const dados = require("./dados.json");
 
 const client = new Client({
     intents: [
@@ -25,9 +26,22 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
+    
     if(message.author.bot) return;
 
     if(message.content === 'ping') message.channel.send(`O ping do bot é de estimados ${client.ws.ping}ms`);
+
+    //gerador de piada aleatorias
+    if(message.content === "!piada"){
+        const random = Math.floor(Math.random() * dados.piada.length);
+        message.channel.send(dados.piada[random])
+    }
+
+    if(message.content === "!frase"){
+        const random = Math.floor(Math.random() * dados.frases.length);
+        message.channel.send(dados.frases[random])
+    }
+
 });
 
 client.login(process.env.TOKEN);
